57b3b3666bde2fa7356c2df12cb727ff4e2aaf07
fyookball <jonaldfyookball@outlook.com>
2017-07-30 14:43:59 -0400

    custom files for android build

---
+++ contrib/jonald/PythonUtil.java
@@ -0,0 +1,67 @@
+package org.kivy.android;
+
+import java.io.File;
+
+import android.util.Log;
+
+
+public class PythonUtil {
+	private static final String TAG = "PythonUtil";
+
+	protected static String[] getLibraries() {
+        return new String[] {
+            "SDL2",
+            "SDL2_image",
+            "SDL2_mixer",
+            "SDL2_ttf",
+	    "m",
+	    "z",
+	    "dl",
+	    "c",
+	    "ssl1.0.2h",
+	    "crypto1.0.2h",
+            "python2.7",
+            "python3.5m",
+            "main"
+        };
+    }
+
+	public static void loadLibraries(File filesDir) {
+
+        String filesDirPath = filesDir.getAbsolutePath();
+        boolean foundPython = false;
+
+		for (String lib : getLibraries()) {
+		    try {
+                System.loadLibrary(lib);
+                if (lib.startsWith("python")) {
+                    foundPython = true;
+                }
+            } catch(UnsatisfiedLinkError e) {
+                // If this is the last possible libpython
+                // load, and it has failed, give a more
+                // general error
+                if (lib.startsWith("python3.6") && !foundPython) {
+                    throw new java.lang.RuntimeException("Could not load any libpythonXXX.so");
+                }
+                continue;
+            }
+        }
+
+        try {
+            System.load(filesDirPath + "/lib/python2.7/lib-dynload/_io.so");
+            System.load(filesDirPath + "/lib/python2.7/lib-dynload/unicodedata.so");
+        } catch(UnsatisfiedLinkError e) {
+            Log.v(TAG, "Failed to load _io.so or unicodedata.so...but that's okay.");
+        }
+        
+        try {
+            // System.loadLibrary("ctypes");
+            System.load(filesDirPath + "/lib/python2.7/lib-dynload/_ctypes.so");
+        } catch(UnsatisfiedLinkError e) {
+            Log.v(TAG, "Unsatisfied linker when loading ctypes");
+        }
+
+        Log.v(TAG, "Loaded everything!");
+    }
+}
---
+++ contrib/jonald/readme.txt
@@ -0,0 +1,1 @@
+PythonUtil.java is a file within python-for-android.  It is modified to specifically include packages.  This fixes the libpythonXXX.so not found error at runtime for Android.
